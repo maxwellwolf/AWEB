651c70f608ef13d06f72e8281911a470916f6b83
laboratorio <laboratorio@EP1434.academico.projecao>
2018-06-05 20:12:42 -0300

    API MAPS

--- index.php
+++ index.php
@@ -13,5 +13,14 @@ and open the template in the editor.
         <?php
         echo "Maxwell Machado";
         ?>
+        <h1>Teste de Uso do Google Maps </h1>
+        <iframe
+         width="600"
+         height="450"
+         frameborder="0" style="border:0"
+         src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD-yYVAAC26iiGr0hnx2-tnLQRfGX7H0vU
+         &q=-15.793601, -47.934001" allowfullscreen>
+        </iframe>
     </body>
 </html>
+<!-- AIzaSyD-yYVAAC26iiGr0hnx2-tnLQRfGX7H0vU -->
